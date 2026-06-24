f7399cb35815b647775ddfc40f947edf02ce4ce1
alejespinozar-ctrl <alej.espinozar@duocuc.cl>
2026-06-24 18:57:40 -0400

    menu principal y definicion de diccionario

---
+++ Subir_nota_pruebapy_Alejandro_Espinoza.py
@@ -0,0 +1,10 @@
+def mostar_menu():
+    print(f"==MENU PRINCIPAL===\n1. Agregar consola\n2. Buscar consola por sigla\n3. Eliminar consola\n4. Mostrar todas las consolas\n5. Salir")
+
+def leer_op():
+    opcion = input("Ingrese una opcion: ").strip()
+    return opcion
+
+def dicion():
+    dicc_consolas = {}
+    dicc_ventas = {}
