979033cdb08a3d47f749d12280f3b5913f792f01
alejespinozar-ctrl <alej.espinozar@duocuc.cl>
2026-06-24 19:04:06 -0400

    bucle y distintas opciones del menu validas

--- Subir_nota_pruebapy_Alejandro_Espinoza.py
+++ Subir_nota_pruebapy_Alejandro_Espinoza.py
@@ -8,3 +8,23 @@ def leer_op():
 def dicion():
     dicc_consolas = {}
     dicc_ventas = {}
+        
+    while True:
+        mostar_menu()
+        op = leer_op()
+        
+        if op == "1":
+            pass
+        elif op == "2":
+            pass
+        elif op == "3":
+            pass
+        elif op == "4":
+            pass
+        elif op == "5":
+            print("\nSaliendo del sistema\n¡Esperamos que vulva pronto!")
+            break
+        else:
+            print("Opción inválida. Ingrese un número del 1 al 5.")
+if __name__ == "__main__":
+    dicion()
